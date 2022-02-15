c6cbe481ed93f3b0bed1628cd4b0dcbb4762667c
anurag kumar singh <54276875+anurag-bit@users.noreply.github.com>
2022-02-15 21:08:43 +0530

    Initial commit

---
+++ components/About/About.component.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { HiOutlineArrowNarrowRight } from "../Icons";
+
+const About = () => {
+  return (
+    <div className="my-8 flex flex-row items-center justify-between px-3 font-sen">
+      <div>
+        <p className="text-3xl font-bold text-white">Anurag</p>
+        <p className="mt-1 text-lg font-medium text-gray-300">
+          Ai Enthusiast, Blogger and a Student
+        </p>
+
+        <p className="mt-4 text-white">
+          Building Web apps, proficient in ML. Writing articles <br /> on
+          my blog sometimes.
+        </p>
+
+        <Link href="https://medium.com/@anuragkumarsingh086" passHref>
+          <a
+            className="mt-4 flex cursor-pointer flex-row items-center gap-1 font-jost text-xl text-white-400 duration-100 hover:ml-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read my Articles...❤️
+            <HiOutlineArrowNarrowRight />
+          </a>
+        </Link>
+
+        <Link href="https://www.instagram.com/prof.freakenstein/" passHref>
+          <a
+            className="mt-4 flex cursor-pointer flex-row items-center gap-1 font-jost text-xl text-white-400 duration-100 hover:ml-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram? Dm me!
+            <HiOutlineArrowNarrowRight />
+          </a>
+        </Link>
+        <p className="mt-4 text-white">
+          Have a radical Day Ahead!!🐯
+        </p>
+      </div>
+
+      <div>
+        <Image
+          src="/assests/avatar.png"
+          width="112"
+          height="112"
+          className="rounded-full"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/_app.tsx
@@ -0,0 +1,66 @@
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+      <NextSeo
+        title="Anurag | AI Enthusiast"
+        titleTemplate="Anurag | AI Enthusiast"
+        defaultTitle="Anurag | AI Enthusiast"
+        description="Hey! I'm Anurag, An AI Enthusiast, Blogger and a Student!"
+        openGraph={{
+          url: "https://www.anurag.tech/",
+          title: "Anurag | Ai Enthusiast",
+          description:
+            "Hey! I'm Anurag, A AI Developer, Blogger and a Student!",
+          images: [
+            {
+              url: "https://res.cloudinary.com/anurag256k/image/upload/v1644938293/wp10293345-think-plan-execute-wallpapers_zdx8qc.jpg",
+              width: 800,
+              height: 420,
+              alt: "Anurag | AI Enthusiast",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@AnuragS24171420",
+          site: "@AnuragS24171420",
+          cardType: "summary_large_image",
+        }}
+        additionalMetaTags={[
+          {
+            property: "keywords",
+            content:
+              "Frontend Developer, anurag, prof.freakenstein, Web Developer, web development, web developer, blogger, tech enthusiast, open source",
+          },
+        ]}
+      />
+      <Head>
+        <link rel="icon" type="image/png" href="/assests/avatar.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
