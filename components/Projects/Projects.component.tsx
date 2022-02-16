f1a1e9e4798d495aefa6ba3c3c4a76ce161f208a
Anurag <singh.ask@outlook.com>
2022-02-16 21:18:59 +0530

    Update Projects.component.tsx
    updated orientation

--- components/Projects/Projects.component.tsx
+++ components/Projects/Projects.component.tsx
@@ -32,12 +32,10 @@ const Projects: React.FC = () => {
             </div>
           </a>
         </Link>
-        <div> 
-          <p>
+        <div>
           <a>
           <iframe  width="400" height="300" frameBorder="0" src="https://cdn.abowman.com/widgets/fish/fish.html?up_backgroundColor=701a75&up_numFish=5&up_fishColor1=f45540&up_fishColor2=c06054&up_fishColor3=f4ce43&up_fishColor4=43f446&up_fishColor5=43a7f4&up_fishColor6=f45540&up_fishColor7=f45540&up_fishColor8=f45540&up_fishColor9=f45540&up_fishColor10=f45540"></iframe>
           </a>
-          </p>
         </div>
       </div>
     </div>
